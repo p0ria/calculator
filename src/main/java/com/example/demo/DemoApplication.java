127ad86a777091a72fa5da197a2fed7aa59fb5d0
p0ria2 <poria.mast3r@gmail.com>
2022-06-03 11:44:31 +0430

    add static-analysis error

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	private final static String constant = "constant";
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
